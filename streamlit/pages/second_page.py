50ad94e11648de73ad312bbecda515a4cbf268f7
Toby Gaskell <Toby96@sky.com>
2025-04-23 18:19:31 +0100

    this is the addition of a second file

---
+++ streamlit/pages/second_page.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Second Page")
+
+st.write(
+    """This is the second page of the Streamlit app. The first page is the 
+    main page.
+       This page is a sub-page that can be accessed from the main page.
+       The main page is the first page that is displayed when the app is 
+       opened.
+    """
+)
+
