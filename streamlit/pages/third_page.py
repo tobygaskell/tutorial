d00138302758ea6a48e11f5178b1b1713b53c3e3
Toby Gaskell <Toby96@sky.com>
2025-04-24 16:25:24 +0100

    saying hello to cdi

--- streamlit/pages/third_page.py
+++ streamlit/pages/third_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Third Page")
 
 if st.button('Click for Fun!', use_container_width=True):
-    st.balloons()
+    st.write('Hello CDI')
 
 if st.button('Back', use_container_width=True):
     st.switch_page('pages/second_page.py')
